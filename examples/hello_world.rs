9b2281f6586392ed82a4d1ae2a52bb7e901428aa
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2024-03-26 02:22:05 +0530

    Create hello_world.rs

---
+++ examples/hello_world.rs
@@ -0,0 +1,10 @@
+use rohanasan::{rohanasan, send_http_response, serve, Request, DEFAULT_HTML_HEADER};
+fn handle(req: Request) -> String {
+    send_http_response(DEFAULT_HTML_HEADER, "<h1>Hello!</h1>", req.data)
+}
+
+fn main() {
+    rohanasan! {
+        serve(8080, handle)
+    }
+}
